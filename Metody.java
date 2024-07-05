214719f77419cca0c9b2067ef47ef117aef90f37
Skamarc <134639253+Skamarc@users.noreply.github.com>
2024-07-05 17:39:00 +0200

    Test Commita 5

--- Metody.java
+++ Metody.java
@@ -1,14 +1,36 @@
 public class Metody {
 
-    public int policzWynik(){
+//    public int policzWynik(){
+//        System.out.println("Policze wynik: ");
+//        int result = 0;
+//        for (int i = 0; i < 100; i++){
+//            System.out.println("Wynik pierwszy to: " + result);
+//            result = result + i;
+//            System.out.println("Wynik drugi(po) to: " + result);
+//        }
+//        System.out.println("Wynik drugi(po) to: " + result);
+//        return result;
+//    }
+
+    public int policzWynikParam(int number){
+        System.out.println("Number ma wartosc: "+ number);
         System.out.println("Policze wynik: ");
         int result = 0;
-        for (int i = 0; i < 100; i++){
-            System.out.println("Wynik pierwszy to: " + result);
+        for (int i = 0; i < number; i++){
+
             result = result + i;
-            System.out.println("Wynik drugi(po) to: " + result);
+
         }
-        System.out.println("Wynik drugi(po) to: " + result);
+        System.out.println("Wynik to: " + result);
         return result;
+    }
+
+    public int add(int firstNumber, int secondNumber) {
+        System.out.println("Wynik Add to: "+ (firstNumber+secondNumber));
+        if(firstNumber == 0 ){
+            System.out.println("First number to 0");
+            return 0;
+        }
+        return firstNumber + secondNumber;
     }
 }
--- MetodyTest.java
+++ MetodyTest.java
@@ -1,9 +1,18 @@
 public class MetodyTest {
     public static void main(String[] args) {
-        Metody Obliczenia = new Metody();
-        Obliczenia.policzWynik();
+        Metody metody = new Metody();
 
-        int Wynik = Obliczenia.policzWynik();
-        System.out.println("Finalny wynik to: " + Wynik);
+//        Obliczenia.policzWynik();
+
+//        int Wynik = Obliczenia.policzWynik();
+//        int Wynik2 = Wynik*2;
+//        System.out.println("Finalny wynik to: " + Wynik);
+//        System.out.println("Wynik po mnozeniu to : "+ Wynik2);
+
+        metody.policzWynikParam(2);
+        metody.policzWynikParam(100);
+        metody.policzWynikParam(23);
+        metody.add(2,6);
+
     }
 }
