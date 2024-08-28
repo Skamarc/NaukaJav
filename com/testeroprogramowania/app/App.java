c1ae2cc5c84d9e2c7e6626ed4a6fa72ff372f477
Skamarc <134639253+Skamarc@users.noreply.github.com>
2024-08-28 19:01:07 +0200

    Commit nr x - Dodanie overloading - Dodanie overriding

---
+++ com/testeroprogramowania/app/App.java
@@ -0,0 +1,15 @@
+package com.testeroprogramowania.app;
+
+import com.testeroprogramowania.app.database.DatabaseConnection;
+import com.testeroprogramowania.app.database.DatabaseUtilis;
+import com.testeroprogramowania.pages.DatabaseUtils;
+
+public class App {
+    public static void main(String[] args) {
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        DatabaseUtilis databaseUtilis = new DatabaseUtilis();
+        DatabaseUtils databaseUtils1 = new DatabaseUtils();
+
+
+    }
+}
---
+++ com/testeroprogramowania/pages/DatabaseUtils.java
@@ -0,0 +1,4 @@
+package com.testeroprogramowania.pages;
+
+public class DatabaseUtils {
+}
